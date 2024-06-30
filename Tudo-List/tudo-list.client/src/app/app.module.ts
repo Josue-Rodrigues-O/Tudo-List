8131fa3040b2b2961d9dbf02a2ded2481c090818
Josué Rodrigues de Oliveira <teste@gmail.com>
2024-06-30 17:19:42 -0300

    Inicio da tela sign-in/sign-up

--- Tudo-List/tudo-list.client/src/app/app.module.ts
+++ Tudo-List/tudo-list.client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInSignUpComponent } from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInSignUpComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ Tudo-List/tudo-list.client/src/app/pages/sign-in-sign-up/sign-in-sign-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-in-sign-up',
+  templateUrl: './sign-in-sign-up.component.html',
+  styleUrl: './sign-in-sign-up.component.css'
+})
+export class SignInSignUpComponent {
+
+}
