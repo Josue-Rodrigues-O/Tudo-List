71bcd2e3735bd8fffa7fd4180817b89ecbd7ebb8
Lucas <lucas.lglesias@inventsoftware.com.br>
2024-07-01 15:41:43 -0300

    feat: adding property UserName to User and migrations

--- Tudo-List.Domain/Entities/User.cs
+++ Tudo-List.Domain/Entities/User.cs
@@ -6,6 +6,7 @@ namespace Tudo_List.Domain.Entities
     public class User
     {
         public int Id { get; set; }
+        public string UserName { get; set; }
         public string Name { get; set; }
 
         [JsonIgnore]
