8e6e2b230e7a19747fe28b0aad85c49149401c94
Lucas <lucas.lglesias@inventsoftware.com.br>
2024-07-01 15:47:29 -0300

    chore: adding automapper to RegisterRequest and UpdateRequest classes

---
+++ Tudo-List.Application/Mappers/RequestToModelMappingUser.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Tudo_List.Application.Models.Users;
+using Tudo_List.Domain.Entities;
+
+namespace Tudo_List.Application.Mappers
+{
+    public class RequestToModelMappingUser : Profile
+    {
+        public RequestToModelMappingUser()
+        {
+            CreateMap<RegisterRequest, User>();
+
+            CreateMap<UpdateRequest, User>()
+                .ForAllMembers(x => x.Condition(
+                    (src, dest, prop) =>
+                    {
+                        var isInvalidString = (prop.GetType() == typeof(string) && string.IsNullOrWhiteSpace((string)prop));
+
+                        if (prop is null)
+                            return false;
+
+                        if (isInvalidString)
+                            return false;
+
+                        return true;
+                    }));
+        }
+    }
+}
