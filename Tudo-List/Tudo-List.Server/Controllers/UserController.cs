4182b3528f0008716bd8715eae270ce3b05bc102
Lucas <lucas.lglesias@inventsoftware.com.br>
2024-07-01 15:49:19 -0300

    chore: implement UserController with all CRUD methods necessary

---
+++ Tudo-List/Tudo-List.Server/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tudo_List.Domain.Core.Interfaces.Services;
+
+namespace Tudo_List.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService userService) : ControllerBase
+    {
+        private readonly IUserService _userService = userService;
+    }
+}
