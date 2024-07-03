d102fbf8e5d230c857fb8de7ac1c5872fd2b35a8
Josué Rodrigues de Oliveira <josue.rodrigues@inventsoftware.com.br>
2024-07-03 14:43:21 -0300

    Correção na animação e implementação do i18n

--- Tudo-List/tudo-list.client/src/app/app.module.ts
+++ Tudo-List/tudo-list.client/src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInSignUpComponent } from './pages/sign-in-sign-up/sign-in-sign-up.component';
 import { TudoListComponent } from './pages/tudo-list/tudo-list.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +23,14 @@ import { TudoListComponent } from './pages/tudo-list/tudo-list.component';
     BrowserModule, HttpClientModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Tudo-List/tudo-list.client/src/app/pages/sign-in-sign-up/sign-in-sign-up.component.ts
+++ Tudo-List/tudo-list.client/src/app/pages/sign-in-sign-up/sign-in-sign-up.component.ts
@@ -1,58 +1,45 @@
 import { Component } from '@angular/core';
 import { User } from '../../models/user/user';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-in-sign-up',
   templateUrl: './sign-in-sign-up.component.html',
-  styleUrl: './sign-in-sign-up.component.css'
+  styleUrl: './sign-in-sign-up.component.css',
 })
 export class SignInSignUpComponent {
   user: User = new User();
   isSignIn: boolean = true;
   canChangeControls: boolean = false;
-  classSignIn: string = 'form-sign-in';
-  classImg: string = '';
 
-  title: string = 'ACCOUNT LOGIN';
-  button: string = 'SIGN IN';
-  signUpSignIn: string = "Don't have an account?";
-  actionSignUpSignIn: string = "Sign up";
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['en', 'pt']);
+    this.translate.setDefaultLang('en');
 
-  onClickTextSignUpSignIn() {
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(
+      browserLang && browserLang.match(/en|pt/) ? browserLang : 'en'
+    );
+  }
+
+  onClickTextSignUpSignIn(form: HTMLElement, image: HTMLElement) {
+    if (this.isSignIn) {
+      form.className = 'form-sign-in animate-form-right';
+      image.className = 'animate-img-left';
+    } else {
+      form.className = 'form-sign-in animate-form-left';
+      image.className = 'animate-img-right';
+    }
     setTimeout(() => {
       this.isSignIn = !this.isSignIn;
-      if (!this.isSignIn) {
-        this.title = 'REGISTER';
-        this.button = 'SIGN UP';
-        this.signUpSignIn = 'Already has an account?';
-        this.actionSignUpSignIn = "Sign in";
-      } else {
-        this.title = 'ACCOUNT LOGIN';
-        this.button = 'SIGN IN';
-        this.signUpSignIn = "Don't have an account?";
-        this.actionSignUpSignIn = "Sign up";
-      }
-    }, 400);
-    var newClassForm: string = 'form-sign-in';
-    var newClassimg: string = 'form-sign-in';
-    if (this.isSignIn) {
-      newClassForm = `${newClassForm} animate-form-right`;
-      newClassimg += ' ' + 'animate-img-left';
-    } else {
-      newClassForm += ' ' + 'animate-form-left';
-      newClassimg += ' ' + 'animate-img-right';
-    }
-    this.classSignIn = newClassForm;
-    this.classImg = newClassimg;
+    }, 500);
   }
 
   onClickButtonSigninSignUp() {
     console.log(this.user);
-
   }
 
   estilizarLabel(evento: any, value: any) {
-    if (!value)
-      evento.classList.toggle('estilo-label')
+    if (!value) evento.classList.toggle('estilo-label');
   }
 }
