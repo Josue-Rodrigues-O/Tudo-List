b963e6ed21292c4663e4767607b56cafcf0c664d
Lucas <lucas.lglesias@inventsoftware.com.br>
2024-08-01 23:35:52 -0300

    refactor: decoupling the generation of problem details logic and letting the code more readable and mantainable

--- Tudo-List/Tudo-List.Server/Extensions/ProblemDetailsExtensions.cs
+++ Tudo-List/Tudo-List.Server/Extensions/ProblemDetailsExtensions.cs
@@ -3,89 +3,33 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Tudo_List.Domain.Exceptions;
-using DtoValidationException = System.ComponentModel.DataAnnotations.ValidationException;
+using Tudo_List.Server.Helpers;
 
 namespace Tudo_List.Server.Extensions
 {
     public static class ProblemDetailsExtensions
     {
-        private static readonly Dictionary<Type, int> StatusCodeByExceptionType = new()
-        {
-            { typeof(ArgumentNullException), StatusCodes.Status500InternalServerError },
-            { typeof(ArgumentException), StatusCodes.Status500InternalServerError },
-            { typeof(ArgumentOutOfRangeException), StatusCodes.Status400BadRequest },
-            { typeof(BadHttpRequestException), StatusCodes.Status400BadRequest },
-            { typeof(DtoValidationException), StatusCodes.Status400BadRequest },
-            { typeof(ValidationException), StatusCodes.Status400BadRequest },
-            { typeof(EntityNotFoundException), StatusCodes.Status404NotFound },
-            { typeof(UnauthorizedAccessException), StatusCodes.Status401Unauthorized },
-        };
-
-        private const string DefaultValidationErrorTitle = "Validation Failed!";
-        private const string DefaultValidationErrorDetail = "Please refer to the errors property for additional details";
-
         public static void UseProblemDetailsExceptionHandler(this IApplicationBuilder app)
         {
-
             app.UseExceptionHandler(builder =>
             {
                 builder.Run(async context =>
                 {
                     var exceptionHandlerFeature = context.Features.Get<IExceptionHandlerFeature>();
 
-                    var jsonSettings = new JsonSerializerSettings
-                    {
-                        ContractResolver = new CamelCasePropertyNamesContractResolver()
-                    };
-
                     if (exceptionHandlerFeature is not null)
                     {
                         var exception = exceptionHandlerFeature.Error;
-                        int statusCode = StatusCodeByExceptionType.GetValueOrDefault(exception.GetType(), StatusCodes.Status500InternalServerError);
 
-                        if (exception is ValidationException validationException)
-                        {
-                            var problemDetails = new ValidationProblemDetails
-                            {
-                                Status = statusCode,
-                                Instance = context.Request.HttpContext.Request.Path,
-                            };
+                        var problemDetails = exception is ValidationException validationException
+                            ? ProblemDetailsHelper.GetValidationProblemDetails(validationException, context)
+                            : ProblemDetailsHelper.GetProblemDetails(exception, context);
 
-                            if (validationException.Errors.Any())
-                            {
-                                problemDetails.Title = DefaultValidationErrorTitle;
-                                problemDetails.Detail = DefaultValidationErrorDetail;
-                                problemDetails.Errors = validationException.Errors.ToDictionary(x => x.PropertyName, x => new string[] { x.ErrorMessage }); ;
-                            }
-                            else
-                            {
-                                problemDetails.Title = validationException.Message;
-                                problemDetails.Detail = validationException.StackTrace;
-                            }
-
-                            context.Response.StatusCode = problemDetails.Status.Value;
-                            context.Response.ContentType = "application/problem+json";
-
-                            var json = JsonConvert.SerializeObject(problemDetails, jsonSettings);
-                            await context.Response.WriteAsync(json);
-                        }
-                        else
-                        {
-                            var problemDetails = new ProblemDetails
-                            {
-                                Title = exception.Message,
-                                Status = statusCode,
-                                Instance = context.Request.HttpContext.Request.Path,
-                                Detail = exception.StackTrace
-                            };
-
-                            context.Response.StatusCode = problemDetails.Status.Value;
-                            context.Response.ContentType = "application/problem+json";
-
-                            var json = JsonConvert.SerializeObject(problemDetails, jsonSettings);
-                            await context.Response.WriteAsync(json);
-                        }
+                        context.Response.StatusCode = problemDetails.Status.Value;
+                        context.Response.ContentType = "application/problem+json";
+                        
+                        string json = JsonConvert.SerializeObject(problemDetails, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+                        await context.Response.WriteAsync(json);
                     }
                 });
             });
@@ -97,15 +41,7 @@ namespace Tudo_List.Server.Extensions
             {
                 options.InvalidModelStateResponseFactory = context =>
                 {
-                    var problemDetails = new ValidationProblemDetails(context.ModelState)
-                    {
-                        Title = DefaultValidationErrorTitle,
-                        Instance = context.HttpContext.Request.Path,
-                        Status = StatusCodeByExceptionType[typeof(DtoValidationException)],
-                        Detail = DefaultValidationErrorDetail
-                    };
-
-                    return new BadRequestObjectResult(problemDetails)
+                    return new BadRequestObjectResult(ProblemDetailsHelper.GetValidationProblemDetails(context))
                     {
                         ContentTypes = { "application/problem+json", "application/problem+xml" }
                     };
