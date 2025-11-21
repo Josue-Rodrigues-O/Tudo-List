fce4159531e2db07772b660e6e025a898fa1a424
Josué Rodrigues de Oliveira <josue.oliveira@maximatech.com.br>
2025-11-21 15:49:51 -0300

    Criação e configuração da página Not Found + Criação dos components das páginas Login e Register

--- Tudo-List/tudo-list.client/src/app/app-routing.module.ts
+++ Tudo-List/tudo-list.client/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
