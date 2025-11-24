47b17e813693da5892a749a27b68a9ec63d33fc8
Josué Rodrigues de Oliveira <josue.oliveira@maximatech.com.br>
2025-11-24 11:15:03 -0300

    Criadas as telas de Login e Register

--- Tudo-List/tudo-list.client/src/app/pages/login/login.component.ts
+++ Tudo-List/tudo-list.client/src/app/pages/login/login.component.ts
@@ -1,11 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormControl, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatButtonModule } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login/login.service';
+import { LoginRequest } from '../../core/models/login/login-request';
 
 @Component({
   selector: 'app-login',
-  standalone: false,
+  standalone: true,
+  imports: [TranslateModule, MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  hide = signal(true);
+  readonly email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  readonly password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(256),
+  ]);
 
+  constructor(private router: Router, private loginService: LoginService) {
+
+  }
+
+  togglePassword() {
+    this.hide.set(!this.hide());
+  }
+
+  onClickLogin() {
+    if (this.email.valid && this.password.valid) {
+      let user: LoginRequest = {
+        email: this.email.value || '',
+        password: this.password.value || ''
+      };
+      this.login(user);
+    }
+  }
+
+  onClickRegister() {
+    this.router.navigate(['/register']);
+  }
+
+  private login(user: LoginRequest) {
+    this.loginService.Login(user)
+      .subscribe({
+        next: (result) => {
+          localStorage.setItem('token', result.token);
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.error(err.error);
+          alert(err.error.title);
+        }
+      });
+  }
 }
--- Tudo-List/tudo-list.client/src/app/pages/register/register.component.ts
+++ Tudo-List/tudo-list.client/src/app/pages/register/register.component.ts
@@ -1,11 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { UserService } from '../../services/user/user.service';
+import { LoginRequest } from '../../core/models/login/login-request';
+import { RegisterUser } from '../../core/models/user/register-user';
+import { LoginService } from '../../services/login/login.service';
 
 @Component({
   selector: 'app-register',
-  standalone: false,
+  standalone: true,
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
+  imports: [TranslateModule, MatFormFieldModule, MatInputModule, MatIconModule, ReactiveFormsModule, MatButtonModule],
 })
 export class RegisterComponent {
+  hide = signal(true);
+  readonly email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ]);
+  readonly password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(256),
+  ]);
+  readonly confirmPassword = new FormControl('', [
+    Validators.required,
+    Validators.minLength(8),
+    Validators.maxLength(256),
+  ]);
 
+  constructor(
+    private router: Router,
+    private userService: UserService,
+    private loginService: LoginService) {
+
+  }
+
+  togglePassword() {
+    this.hide.set(!this.hide());
+  }
+
+  onClickLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  onClickRegister() {
+    if (this.email.valid && this.password.valid) {
+      let user: RegisterUser = {
+        email: this.email.value || '',
+        password: this.password.value || '',
+        confirmPassword: this.confirmPassword.value || '',
+        name: (this.email.value || '').split('@')[0]
+      };
+
+      this.userService.Register(user)
+        .subscribe({
+          next: () => {
+            this.login(user);
+          },
+          error: (err) => {
+            console.error(err.error);
+            alert(err.error.title);
+          }
+        });
+    }
+  }
+
+  private login(user: LoginRequest) {
+    this.loginService.Login(user)
+      .subscribe({
+        next: (result) => {
+          localStorage.setItem('token', result.token);
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.error(err.error);
+          alert(err.error.title);
+        }
+      });
+  }
 }
